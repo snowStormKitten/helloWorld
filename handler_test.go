783f6711e12aba35139039b508dbb9b2b4db90e8
ADuckPond <ag3rlitz@gmail.com>
2018-09-07 20:44:44 -0400

    removed index.html added go files to test circleci integration with a test.go file. Go app is a simple HTML page that displays a Hello World! message. handler_test.go test the healthcheck function of the app

---
+++ handler_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHealthCheckHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/healthCheck", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	h := http.HandlerFunc(healthCheck)
+
+	h.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", 
+			status, http.StatusOK)
+	}
+
+	expected := `{"alive": true}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", 
+			rr.Body.String(), expected)
+	}
+}
---
+++ helloWorld.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"io"
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+
+	t, _ := template.ParseFiles("layout.html")
+
+	data := struct {
+		Title string
+		Text string
+	}{
+		Title: "Hello World!",
+		Text: "Hello World!",
+	}
+
+	t.Execute(w, data)
+
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/healthCheck", healthCheck)
+
+	http.ListenAndServe(":80", nil)
+}
