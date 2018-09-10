01847ca31b22e26ba76b33793b7f7ebc89dfb142
ADuckPond <ag3rlitz@gmail.com>
2018-09-10 09:00:34 -0400

    updated text contents for demo

--- helloWorld.go
+++ helloWorld.go
@@ -15,7 +15,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 		Text string
 	}{
 		Title: "Hello World!",
-		Text: "Hello World! TEST1",
+		Text: "Hello World!",
 	}
 
 	t.Execute(w, data)
