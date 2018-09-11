6651253a59193e2d6f2f63f8d778536755177950
ADuckPond <ag3rlitz@gmail.com>
2018-09-11 11:07:48 -0400

    Added Test1 text to the Hello World! page

--- helloWorld.go
+++ helloWorld.go
@@ -15,7 +15,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 		Text string
 	}{
 		Title: "Hello World!",
-		Text: "Hello World!",
+		Text: "Hello World! Test1",
 	}
 
 	t.Execute(w, data)
