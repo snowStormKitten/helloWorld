3f0aa8c723a401ea33066c8bbb8d7cf060cecbf2
ADuckPond <ag3rlitz@gmail.com>
2018-10-07 20:32:35 -0400

    reset to hello world

--- helloWorld.go
+++ helloWorld.go
@@ -14,8 +14,8 @@ func handler(w http.ResponseWriter, r *http.Request){
 		Title string
 		Text string
 	}{
-		Title: "XYZ Bank",
-		Text: "Welcome to XYZ Bank!",
+		Title: "Hello World",
+		Text: "Hello World!",
 	}
 
 	t.Execute(w, data)
