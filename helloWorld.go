bf8294e94de34e3510e8b9921be18b9f2e62c54c
ADuckPond <ag3rlitz@gmail.com>
2019-02-20 09:57:18 -0500

    reset hello world and scale down to 3 replicas

--- helloWorld.go
+++ helloWorld.go
@@ -14,8 +14,8 @@ func handler(w http.ResponseWriter, r *http.Request){
 		Title string
 		Text string
 	}{
-		Title: "ABC BANK",
-		Text: "Welcome ABC Bank!",
+		Title: "Hello World!",
+		Text: "Hello World!",
 	}
 
 	t.Execute(w, data)
